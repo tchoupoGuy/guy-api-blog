631b76a5bdf00b0a2217f7a53f7762121e6db767
guytchoupo <guytchoupo@gmail.com>
2021-09-24 13:56:04 -0400

    dded manage user endpoin

--- user/serializers.py
+++ user/serializers.py
@@ -24,6 +24,9 @@ class UserSerializer(serializers.ModelSerializer):
             user.set_password(password)
             user.save()
 
+        return user
+
+
 
 class AuthTokenSerializer(serializers.Serializer):
     """Serializer for the user authentication object"""
